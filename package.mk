9ef1b7397f42c1bff6e0aaa8015a79bb4a05f867
David Wragg <david@rabbitmq.com>
2011-01-20 11:38:12 +0000

    Eliminate wrapper package boilerplate

--- package.mk
+++ package.mk
@@ -1,10 +1,7 @@
 APP_NAME:=rfc4627_jsonrpc
 
 UPSTREAM_GIT:=http://github.com/tonyg/erlang-rfc4627.git
-
-CHECKOUT_DIR:=$(PACKAGE_DIR)/erlang-rfc4627-git
-SOURCE_DIRS+=$(CHECKOUT_DIR)/src
-INCLUDE_DIRS+=$(CHECKOUT_DIR)/include
+UPSTREAM_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
 
 ## The path to httpd.hrl has changed in OTP R14A and newer. Detect the
 ## change, and supply a compile-time macro definition to allow
@@ -14,26 +11,3 @@ ERLC_OPTS:=
 else
 ERLC_OPTS:=-Dnew_inets
 endif
-
-$(eval $(call safe_include,$(PACKAGE_DIR)/version.mk))
-
-VERSION:=rmq$(GLOBAL_VERSION)-git$(COMMIT_SHORT_HASH)
-
-define package_targets
-
-$(CHECKOUT_DIR)/.done:
-	rm -rf $(CHECKOUT_DIR)
-	git clone $(UPSTREAM_GIT) $(CHECKOUT_DIR)
-	touch $$@
-
-$(PACKAGE_DIR)/version.mk: $(CHECKOUT_DIR)/.done
-	echo COMMIT_SHORT_HASH:=`git --git-dir=$(CHECKOUT_DIR)/.git log -n 1 --format=format:"%h" HEAD` >$$@
-
-$(EBIN_DIR)/$(APP_NAME).app: $(CHECKOUT_DIR)/ebin/$(APP_NAME).app $(PACKAGE_DIR)/version.mk
-	@mkdir -p $$(@D)
-	sed -e 's|{vsn, *\"[^\"]*\"|{vsn,\"$(VERSION)\"|' <$$< >$$@
-
-$(PACKAGE_DIR)+clean::
-	rm -rf $(CHECKOUT_DIR) $(EBIN_DIR)/$(APP_NAME).app $(PACKAGE_DIR)/version.mk
-
-endef
