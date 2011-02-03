cfb09a6714baf9a76e29e3edd076c432be225aa3
David Wragg <david@rabbitmq.com>
2011-02-03 11:54:00 +0000

    Unify wrapper and non-wrapper package app file generation

--- package.mk
+++ package.mk
@@ -1,7 +1,7 @@
 APP_NAME:=rfc4627_jsonrpc
 
 UPSTREAM_GIT:=http://github.com/tonyg/erlang-rfc4627.git
-UPSTREAM_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
+ORIGINAL_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
 
 ## The path to httpd.hrl has changed in OTP R14A and newer. Detect the
 ## change, and supply a compile-time macro definition to allow
