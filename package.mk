171c1bbdf74d3b5002d0eb138cb8f85007876d49
David Wragg <david@rabbitmq.com>
2011-02-21 08:32:48 +0000

    ERLC_OPTS -> PACKAGE_ERLC_OPTS

--- package.mk
+++ package.mk
@@ -7,5 +7,5 @@ ORIGINAL_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
 ## change, and supply a compile-time macro definition to allow
 ## rfc4627_jsonrpc_inets.erl to adapt to the new path.
 ifeq ($(shell erl -noshell -eval 'io:format([list_to_integer(X) || X <- string:tokens(erlang:system_info(version), ".")] >= [5,8]), halt().'),true)
-ERLC_OPTS:=-Dnew_inets
+PACKAGE_ERLC_OPTS+=-Dnew_inets
 endif
