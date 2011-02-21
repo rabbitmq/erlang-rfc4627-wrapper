270317016987e1121f5c7fe489e2daf35b464401
David Wragg <david@rabbitmq.com>
2011-02-21 07:38:34 +0000

    Improve erlang version check

--- package.mk
+++ package.mk
@@ -6,8 +6,6 @@ ORIGINAL_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
 ## The path to httpd.hrl has changed in OTP R14A and newer. Detect the
 ## change, and supply a compile-time macro definition to allow
 ## rfc4627_jsonrpc_inets.erl to adapt to the new path.
-ifeq ($(shell test R14A \> $$(erl -noshell -eval 'io:format(erlang:system_info(otp_release)), halt().') && echo yes),yes)
-ERLC_OPTS:=
-else
+ifeq ($(shell erl -noshell -eval 'io:format([list_to_integer(X) || X <- string:tokens(erlang:system_info(version), ".")] >= [5,8]), halt().'),true)
 ERLC_OPTS:=-Dnew_inets
 endif
