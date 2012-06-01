2c6ed9b4fc1c9133c989f3377f08bf2518418184
Simon MacMullen <simon@rabbitmq.com>
2012-06-01 13:03:36 +0100

    Backport ed048e589696 (Merge of bug24975; track httpd.hrl header more accurately)

--- package.mk
+++ package.mk
@@ -2,8 +2,7 @@ APP_NAME:=rfc4627_jsonrpc
 DEPS:=mochiweb-wrapper
 
 UPSTREAM_GIT:=http://github.com/tonyg/erlang-rfc4627.git
-UPSTREAM_REVISION:=30c84984f86a1d67083f
-WRAPPER_PATCHES:=fix-ets-leak.patch
+UPSTREAM_REVISION:=a5e7ad77963f1e7130d8ca870cfd9a7c1a12c8cc
 
 ORIGINAL_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
 DO_NOT_GENERATE_APP_FILE=true
@@ -11,6 +10,10 @@ DO_NOT_GENERATE_APP_FILE=true
 ## The path to httpd.hrl has changed in OTP R14A and newer. Detect the
 ## change, and supply a compile-time macro definition to allow
 ## rfc4627_jsonrpc_inets.erl to adapt to the new path.
-ifeq ($(shell erl -noshell -eval 'io:format([list_to_integer(X) || X <- string:tokens(erlang:system_info(version), ".")] >= [5,8]), halt().'),true)
-PACKAGE_ERLC_OPTS+=-Dnew_inets
+ifeq ($(shell erl -noshell -eval 'io:format([list_to_integer(X) || X <- string:tokens(erlang:system_info(version), ".")] < [5,8]), halt().'),true)
+PACKAGE_ERLC_OPTS+=-Dinets_pre_r14a
+else
+ifeq ($(shell erl -noshell -eval 'io:format([list_to_integer(X) || X <- string:tokens(erlang:system_info(version), ".")] < [5,8,2]), halt().'),true)
+PACKAGE_ERLC_OPTS+=-Dinets_pre_r14b01
+endif
 endif
